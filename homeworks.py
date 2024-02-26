070b0373f1062bb79b03bb80d63f5047a2bba6d1
Vigen <vigengukasyan@gmail.com>
2024-02-26 21:23:33 +0400

    Number Classification (Lecture_4)

--- homeworks.py
+++ homeworks.py
@@ -1,24 +1,36 @@
-    #Calculator
+#     #Calculator
 
-a = float(input(" "))
-operation = input()
-b = float(input(" "))
-if operation == '+':
-    result = a + b
-elif operation == '-': 
-    result = a - b
-elif operation == '*':
-    result = a * b
-elif operation == '/':
-    if b != 0:
-        result = a / b
-    else:
-        result = "Error! Division by zero."
-else:
-    result = "Invalid operation"
+# a = float(input(" "))
+# operation = input()
+# b = float(input(" "))
+# if operation == '+':
+#     result = a + b
+# elif operation == '-': 
+#     result = a - b
+# elif operation == '*':
+#     result = a * b
+# elif operation == '/':
+#     if b != 0:
+#         result = a / b
+#     else:
+#         result = "Error! Division by zero."
+# else:
+#     result = "Invalid operation"
 
-print("result = ", result, sep = "", end='\n')
+# print("result = ", result, sep = "", end='\n')
 
 
+#Number Classification
+
+nums = list(map(int, input("Enter a list of numbers separated by spaces: ").split()))
+even = []
+odd = []
+for i in nums:
+    if i % 2 == 0:
+        even.append(i)
+    else: 
+        odd.append(i)
+print("Even numbers: ", even, end = '\n')
+print("Odd numbers: ", odd) 
    
 
