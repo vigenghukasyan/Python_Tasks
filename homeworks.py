9c28ef1e36176792102c071468911925576f2c91
Vigen <vigengukasyan@gmail.com>
2024-03-01 21:00:56 +0400

    Matrix Operations (Lecture_6)

--- homeworks.py
+++ homeworks.py
@@ -36,28 +36,75 @@
 
     #Sum of Elements
 
-def sum_of_elements(numbers, exclude_negative=False):
-    sum = 0
-    for num in numbers:
-        if not exclude_negative or num >= 0:
-            sum += num
-    return sum
+# def sum_of_elements(numbers, exclude_negative=False):
+#     sum = 0
+#     for num in numbers:
+#         if not exclude_negative or num >= 0:
+#             sum += num
+#     return sum
 
-def main():
+# def main():
 
-    input_numbers = input("Enter a list of numbers separated by spaces: ")
+#     input_numbers = input("Enter a list of numbers separated by spaces: ")
     
-    numbers = [int(num) for num in input_numbers.split()]
+#     numbers = [int(num) for num in input_numbers.split()]
     
-    exclude = input("Do you want to exclude negative numbers? (yes/no): ").lower()
-    exclude_negative = exclude.startswith('y')
+#     exclude = input("Do you want to exclude negative numbers? (yes/no): ").lower()
+#     exclude_negative = exclude.startswith('y')
     
-    total = sum_of_elements(numbers, exclude_negative)
+#     total = sum_of_elements(numbers, exclude_negative)
     
-    print("Sum of the elements:", total)
+#     print("Sum of the elements:", total)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+    #Matrix Operations
+
+import random
+
+def generate_random_matrix(rows, cols):
+    matrix = []
+    for _ in range(rows):
+        row = [random.randint(1, 100) for _ in range(cols)]
+        matrix.append(row)
+    return matrix
+
+def get_column_sum(matrix, column_index):
+    column_sum = 0
+    for row in matrix:
+        column_sum += row[column_index]
+    return column_sum
+
+def get_row_average(matrix, row_index):
+    row = matrix[row_index]
+    row_sum = sum(row)
+    row_average = row_sum / len(row)
+    return row_average
+
+rows = int(input("rows: "))
+cols = int(input("cols: "))
+matrix = generate_random_matrix(rows, cols)
+print("Generated Matrix:")
+for row in matrix:
+    print(row)
+
+column_index = int(input("Column index: "))
+while column_index >= cols:
+    print("ERROR: The column index cant be bigger than the number of cols!")
+    column_index = int(input("Column index: "))
+
+column_sum = get_column_sum(matrix, column_index)
+print("Sum of values in column", column_index, ":", column_sum)
+
+row_index = int(input("Row index: "))
+while row_index >= rows:
+    print("ERROR: The row index cant be bigger than the number of rows!")
+    row_index = int(input("Row index: "))
+
+row_average = get_row_average(matrix, row_index)
+print("Average of values in row", row_index, ":", row_average)
 
    
 
